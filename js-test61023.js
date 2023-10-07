1b59f8687086a256720447049f90a90958b0d487
Hussein Samaha <121319771+intb78@users.noreply.github.com>
2023-10-07 17:42:48 +0300

    add on javascript

---
+++ js-test61023.js
@@ -0,0 +1,6 @@
+function myFunction () {
+let person = prompt("Please Enter your name"); 
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + person + "! How are you today?";
+}}
